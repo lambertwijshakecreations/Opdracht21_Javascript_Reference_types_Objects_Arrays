2db93d76e9172d65de89fe87bfd1757a422323aa
lambertwijshakecreations <73013216+lambertwijshakecreations@users.noreply.github.com>
2021-01-24 21:04:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+/*
+//deel 1
+
+let person = {
+  Name: "Lambert",
+  age: 38
+  evaluations:[7,10, 9]
+};
+
+
+
+console.log(person);
+console.log(person.Name);
+console.log(person.age);
+
+person.name ="Robert";
+console.log(person.name);
+
+person['name'] = 'tony';
+console.log(person.name);
+
+console.log(person.evaluations);
+*/
+
+/*
+// deel 2
+
+let selectedColors = ['green','blue','red']
+console.log(selectedColors); //dit laat alle kleuren zien
+
+console.log(selectedColors.length); // dit laat het aantal arrays zien
+
+console.log(selectedColors[0]); // dit is de eerste array
+
+console.log(selectedColors[2]); // dit is de derde array
+
+selectedColors[3] ='yellow';
+selectedColors[4] = 5;
+
+console.log(selectedColors);
+
+selectedColors[5] = {greeting: "Hi ik ben een object"};
+console.log(selectedColors);
+*/
+
+// deel 3
+
+console.log( "American Bobtail");
+console.log(5);
+console.log( "Affectionate");
+console.log( "Sensitive");
+console.log( "meaty things");
